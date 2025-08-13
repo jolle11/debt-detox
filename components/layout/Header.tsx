65afe5ee69c6f2c1b66eccd1ea2203276d89665a
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2025-08-13 08:40:33 +0200

    feat: add header

---
+++ components/layout/Header.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import LanguageSelector from "@/components/language-selector";
+import ThemeToggle from "@/components/ui/ThemeToggle";
+
+export default function Header() {
+	const t = useTranslations();
+
+	return (
+		<header className="navbar bg-base-200 shadow-lg">
+			<div className="flex-1">
+				<h1 className="btn btn-ghost text-xl font-bold">
+					{t("nav.title")}
+				</h1>
+			</div>
+			<div className="flex-none flex gap-2">
+				<LanguageSelector />
+				<button className="btn btn-primary btn-sm">
+					+ {t("nav.addDebt")}
+				</button>
+				<ThemeToggle />
+			</div>
+		</header>
+	);
+}
