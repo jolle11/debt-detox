201b9eda270a90cdfda85dddd6b403914cb0858d
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2025-08-14 08:57:26 +0200

    style: change header width

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -8,18 +8,20 @@ export default function Header() {
 	const t = useTranslations();
 
 	return (
-		<header className="navbar bg-base-200 shadow-lg">
-			<div className="flex-1">
-				<h1 className="btn btn-ghost text-xl font-bold">
-					{t("nav.title")}
-				</h1>
-			</div>
-			<div className="flex-none flex gap-2">
-				<LanguageSelector />
-				<button className="btn btn-primary btn-sm">
-					+ {t("nav.addDebt")}
-				</button>
-				<ThemeToggle />
+		<header className="bg-base-200 shadow-lg">
+			<div className="navbar container mx-auto">
+				<div className="flex-1">
+					<h1 className="btn btn-ghost text-xl font-bold">
+						{t("nav.title")}
+					</h1>
+				</div>
+				<div className="flex-none flex gap-2">
+					<LanguageSelector />
+					<button className="btn btn-primary btn-sm">
+						+ {t("nav.addDebt")}
+					</button>
+					<ThemeToggle />
+				</div>
 			</div>
 		</header>
 	);
